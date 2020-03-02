f8505856d95621256476ad02fe3601facc08a941
kapuskaFaizan <44909493+kapuskaFaizan@users.noreply.github.com>
2020-03-02 10:10:03 +0530

    Update rule_based_chatbot.py

--- rule_based_chatbot.py
+++ rule_based_chatbot.py
@@ -75,18 +75,23 @@ print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you 
 while(flag==True):
     user_response = input()
     user_response=user_response.lower()
+    
     if(user_response!='bye'):
         if(user_response=='thanks' or user_response=='thank you' ):
             flag=False
             print("ROBO: You are welcome..")
+       
         else:
+            
             if(greeting(user_response)!=None):
                 print("ROBO: "+greeting(user_response))
+            
             else:
                 print("ROBO: ",end="")
                 print(response(user_response))
                 sent_tokens.remove(user_response)
     else:
+        
         flag=False
         print("ROBO: Bye! take care..")    
 
